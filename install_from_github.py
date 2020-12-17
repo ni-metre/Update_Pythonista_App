6529c5f871db811dda6adb562196c9ba5814a399
ni-metre <60623938+ni-metre@users.noreply.github.com>
2020-12-16 17:34:25 -0800

    Update install_from_github.py

--- install_from_github.py
+++ install_from_github.py
@@ -1,5 +1,5 @@
 # Python imports
-import os, requests, shutil, zipfile, json, sys
+import os, requests, shutil, zipfile, json, sys, tempfile, fnmatch
 from types import SimpleNamespace
 from pprint import pprint
 
@@ -18,10 +18,17 @@ CONFIG_DICT = {'install_dir_name': "MetreiOS",
 }
 
 
+TEMPDIR = tempfile.mkdtemp()
 def init_install_path(install_dir_name):
     install_path = os.path.abspath(os.path.expanduser('~/Documents/' + install_dir_name))
-    os.makedirs(install_path, exist_ok=True)
-    return install_path
+    if os.path.exists(install_path):
+    	update = True
+    	print('app directory exists')
+    else:
+    	print('making app directory')
+    	os.makedirs(install_path, exist_ok=True)
+    	update = False
+    return install_path, update
 
 def make_git_url(usr, repo, branch):
     URL_TEMPLATE = 'https://github.com/{}/{}/archive/{}.zip'
@@ -33,8 +40,8 @@ def git_headers(git_pat):
     headers = {"Authorization": token}
     return headers
 
-def install_from_github(install_path, auth_token, url):
-    token_pyld = "token " + auth_token + "a60ab710b075"
+def install_from_github(install_path, auth_token, url, update_status):
+    token_pyld = "token " + auth_token
     headers = {"Authorization": token_pyld}
     dwnld_zipfile = '/'+ url.split('/')[-1]
     local_zipfile = install_path + dwnld_zipfile
@@ -46,22 +53,43 @@ def install_from_github(install_path, auth_token, url):
             for chunk in r.iter_content(block_sz):
                 f.write(chunk)
         z = zipfile.ZipFile(local_zipfile)
-        z.extractall(install_path)
+        z.extractall(TEMPDIR)
+        print('These are the files in the tempdir')
+        githubfolder = os.listdir(TEMPDIR)
+        #print(os.listdir(TEMPDIR))
+        print(githubfolder[0])
+        print('These are the folders in tempdir/githubfolder')
+        tempsource = TEMPDIR + '/' + githubfolder[0]
+        print("THIS is tempsource " + str(tempsource))
+        print("THIS is the destination " + str(install_path))
+        print(os.listdir(tempsource))
+        allFileList = os.listdir(tempsource)
+        for file in allFileList:
+        	if update_status:
+        		if fnmatch.fnmatch(file, 'log'):
+        			print('Keeping existing settings and log data')
+        		else:
+        			shutil.move(tempsource + '/' + file, install_path + '/' + file)
+        	else:
+        		shutil.move(tempsource + '/' + file, install_path + '/' + file)
+
         unzipped_dirname = z.namelist()[0]
         os.remove(local_zipfile)
-        return z.namelist()
+        installedFileList = os.listdir(install_path)
+        print('finished listing')
+        return installedFileList, githubfolder[0]
     except Exception as e:
         print(f"Install Error: {e}")
         return None
 
 def install_branch(params):
-    install_path = init_install_path(params.install_dir_name)
+    install_path, update_status = init_install_path(params.install_dir_name)
     url = make_git_url(params.git_usr, params.git_repo, params.git_branch)
-    installed_files = install_from_github(install_path, params.git_auth, url)
+    installed_files, dirfromgit = install_from_github(install_path, params.git_auth, url, update_status)
     print(f"\nUnzipping: {url}")
     pprint(installed_files)
     print()
-    return install_path, url, installed_files
+    return install_path, url, installed_files, dirfromgit
 
 def create_url_scheme_and_qr_code(installed_dir, url_scheme, start_file):
     url_file = start_file.split('.')[0] + '.url'
@@ -82,28 +110,31 @@ def main():
         pprint(params_dict)
 
         params = SimpleNamespace(**params_dict)
-        install_path, url, installed_files = install_branch(params)
+        install_path, url, installed_files, dirfromgit = install_branch(params)
 
-        start_path = install_path + '/' + installed_files[0].replace('/','') + '/' + params.start_file
+        start_path = install_path + '/' + dirfromgit + '/' + params.start_file
         url_scheme = shortcuts.pythonista_url(path=start_path, action='run', args="", argv=[])
         print(f"\nURL scheme: {url_scheme}")
 
         installed_dir = install_path + '/' + installed_files[0]
         create_url_scheme_and_qr_code(installed_dir, url_scheme, params.start_file)
     else:
+    	
+       
         params = SimpleNamespace(**CONFIG_DICT)
         print("\nInstalling:")
         pprint(CONFIG_DICT)
-        install_path, url, installed_files = install_branch(params)
+        install_path, url, installed_files, dirfromgit = install_branch(params)
 
-        start_path = install_path + '/' + installed_files[0].replace('/','') + '/' + params.start_file
-        url_scheme = shortcuts.pythonista_url(path=start_path, action='run', args="", argv=[])
-        print(f"\nURL scheme: {url_scheme}")
+        start_path = install_path + '/' + dirfromgit + '/' + params.start_file
+        #url_scheme = shortcuts.pythonista_url(path=start_path, action='run', args="", argv=[])
+        #print(f"\nURL scheme: {url_scheme}")
 
-        installed_dir = install_path + '/' + installed_files[0]
-        create_url_scheme_and_qr_code(installed_dir, url_scheme, params.start_file)
+        #installed_dir = install_path + '/' + installed_files[0]
+        #create_url_scheme_and_qr_code(installed_dir, url_scheme, #params.start_file)
 
-        shortcuts.open_url(url_scheme)
+        #shortcuts.open_url(url_scheme)
+       
 
 if __name__ == '__main__':
     main()
